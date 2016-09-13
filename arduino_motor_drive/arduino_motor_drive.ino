b313fc543f16b86588da0f1fb687c6a364a2e8c4
Brandon Jameson <barefooter22@gmail.com>
2016-09-13 17:06:03 -0500

    Paired Arduino with Sabertooth Motor Data topic
    I added code to send the motor controller data over the TX line on the
    Arduino so that it could be sent to the Sabertooth Controller

--- arduino_motor_drive/arduino_motor_drive.ino
+++ arduino_motor_drive/arduino_motor_drive.ino
@@ -6,29 +6,42 @@
 
 #include <ros.h>
 #include <std_msgs/String.h>
+#include <SoftwareSerial.h>
+#define txPin 1 //define the TX pin that will connect to the Saberooth Motor Controller
 
 ros::NodeHandle  nh;
+SoftwareSerial MotorController = SoftwareSerial(0, txPin); //set up the serial on TX pin of Arduino
 
 void messageCb( const std_msgs::String& msg){
   //digitalWrite(13, HIGH-digitalRead(13));   // blink the led
 //  Serial.write("\r\n");
 //  Serial.write(msg.data);
+//    Serial.write("TESTING 123");
+//    MotorController.write("\r\n");
+//    MotorController.write("M1:");//prefix for motor command
+  //  MotorController.write(msg.data); //send motor value
+      Serial.write("M1: ");//prefix for motor command
+      Serial.write(msg.data);
+      Serial.write("\r\n");
+    //Serial.write(msg.data); //send motor value
 }
 
 ros::Subscriber<std_msgs::String> sub("sabertooth_motor_data", &messageCb );
 
 void setup()
 { 
-  pinMode(13, OUTPUT);
   nh.initNode();
   nh.subscribe(sub);
 //  nh.getHardware()->setBaud(9600);//force the baud rate to be set at 9600 (if this has problems, remove this and replace the line below to 57600)
   Serial.begin(9600);
+  
+  pinMode(txPin, OUTPUT);
+  MotorController.begin(9600); //set the baud rate for the tx output to be 9600
 }
 
 void loop()
 {  
   nh.spinOnce();
-  delay(1);
+  delay(10);
 }
 
