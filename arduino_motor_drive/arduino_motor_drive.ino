4ee5802b388d260d3d0e379a68ec7da7bd21f8ea
Brandon Jameson <barefooter22@gmail.com>
2016-09-13 00:29:34 -0500

    Updated information about the file and fixed baud rate
    Fixed baud rate problems and added Title and Description information

--- arduino_motor_drive/arduino_motor_drive.ino
+++ arduino_motor_drive/arduino_motor_drive.ino
@@ -1,6 +1,7 @@
 /* 
- * rosserial Subscriber Example
- * Blinks an LED on callback
+ * Title: Arduino Motor Driver
+ * Description: Arduino file used for taking ROS topic information and sending it to an Arduino to drive a Sabertooth Motor Controller
+ * Author: Brandon Jameson
  */
 
 #include <ros.h>
@@ -10,8 +11,8 @@ ros::NodeHandle  nh;
 
 void messageCb( const std_msgs::String& msg){
   //digitalWrite(13, HIGH-digitalRead(13));   // blink the led
-  Serial.write("\r\n");
-  Serial.write(msg.data);
+//  Serial.write("\r\n");
+//  Serial.write(msg.data);
 }
 
 ros::Subscriber<std_msgs::String> sub("sabertooth_motor_data", &messageCb );
@@ -21,7 +22,7 @@ void setup()
   pinMode(13, OUTPUT);
   nh.initNode();
   nh.subscribe(sub);
-  nh.getHardware()->setBaud(9600);//force the baud rate to be set at 9600 (if this has problems, remove this and replace the line below to 57600)
+//  nh.getHardware()->setBaud(9600);//force the baud rate to be set at 9600 (if this has problems, remove this and replace the line below to 57600)
   Serial.begin(9600);
 }
 
