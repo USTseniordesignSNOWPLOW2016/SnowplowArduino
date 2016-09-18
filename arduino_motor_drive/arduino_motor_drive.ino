b66c70169be1e7d84e3d0dccf8322a5659bc3d8a
Brandon Jameson <barefooter22@gmail.com>
2016-09-18 03:06:57 -0500

    Added second subscriber to take data for motor 2
    I added a second subscriber to the Arduino and a callback function to
    send the messages to the Sabertooth motor controller

--- arduino_motor_drive/arduino_motor_drive.ino
+++ arduino_motor_drive/arduino_motor_drive.ino
@@ -26,12 +26,28 @@ void messageCb( const std_msgs::String& msg){
     //Serial.write(msg.data); //send motor value
 }
 
+void messageCb_2( const std_msgs::String& msg){
+  //digitalWrite(13, HIGH-digitalRead(13));   // blink the led
+//  Serial.write("\r\n");
+//  Serial.write(msg.data);
+//    Serial.write("TESTING 123");
+//    MotorController.write("\r\n");
+//    MotorController.write("M1:");//prefix for motor command
+  //  MotorController.write(msg.data); //send motor value
+      Serial.write("M2: ");//prefix for motor command
+      Serial.write(msg.data);
+      Serial.write("\r\n");
+    //Serial.write(msg.data); //send motor value
+}
+
 ros::Subscriber<std_msgs::String> sub("sabertooth_motor_data", &messageCb );
+ros::Subscriber<std_msgs::String> sub_2("sabertooth_motor_data_2", &messageCb_2 );
 
 void setup()
 { 
   nh.initNode();
   nh.subscribe(sub);
+  nh.subscribe(sub_2);
 //  nh.getHardware()->setBaud(9600);//force the baud rate to be set at 9600 (if this has problems, remove this and replace the line below to 57600)
   Serial.begin(9600);
   
