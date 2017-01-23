bc072f00c872cfe884d50250b215f6d1e0f161f3
Brandon Jameson <barefooter22@gmail.com>
2017-01-22 22:44:50 -0600

    fixed publisher problem

--- ultrasonic_monitor/ultrasonic_monitor.ino
+++ ultrasonic_monitor/ultrasonic_monitor.ino
@@ -15,23 +15,13 @@
 
 /*--------Instantiate node handle-------------*/
 ros::NodeHandle nh;
-// std_msgs::Int32MultiArray sonar;
 plow_waypoint_nav::plow_ultrasonic UltraData; //define a message of type "plow_ultrasonic" that will be used to publish ultrasonic sensor values
-// ros::Publisher sonar_pub("sonar", &sonar);
 ros::Publisher Ultra_pub("/ultrasonic_data", &UltraData); //define a publisher for publishing all of the ultrasonic sensor data
 
 /*-----( Declare objects )-----*/
 NewPing sonar1(TRIGGER_PIN1, ECHO_PIN1, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
 NewPing sonar2(TRIGGER_PIN2, ECHO_PIN2, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
 NewPing sonar3(TRIGGER_PIN3, ECHO_PIN3, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
-/*-----( Declare Variables )-----*/
-int DistanceIn;
-int DistanceCm;
-int Front;
-int Side1;
-int Side2;
-
-
 
 void setup()
 {
@@ -69,7 +59,7 @@ void loop()
   delay(100);
   checkSensor('R');
   delay(100);
-  Ultra_pub.publish(UltraData); //publish the ultrasonic scans
+  Ultra_pub.publish(&UltraData); //publish the ultrasonic scans
   nh.spinOnce();
 }
 
